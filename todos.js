6875f337353eef6e3530ee4f30bb9ac220479d45
Ivan Montiel <idmontie@asu.edu>
2016-01-18 14:36:25 -0700

    Initial step 3

--- todos.js
+++ todos.js
@@ -1,16 +1,17 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
+    tasks: function () {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    //
+    Tasks.insert({ text: "Hello world!", createdAt: new Date() });
+    Tasks.insert({ text: "Give a presentation about Meteor", createdAt: new Date() });
   });
 }
