3130179c8bc1fc767a1c38fdf9a11efeece46250
Ivan Montiel <idmontie@asu.edu>
2016-01-18 14:37:56 -0700

    Initial step 4

--- todos.js
+++ todos.js
@@ -7,11 +7,29 @@ if (Meteor.isClient) {
       return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+ 
+      // Get value from form element
+      var text = event.target.text.value;
+ 
+      // Insert a task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+ 
+      // Clear form
+      event.target.text.value = "";
+    }
+  });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    Tasks.insert({ text: "Hello world!", createdAt: new Date() });
-    Tasks.insert({ text: "Give a presentation about Meteor", createdAt: new Date() });
+    //
   });
 }
